8894681440682aa67c60f807a4bb96d29c589a08
Dave Coleman <davetcoleman@gmail.com>
2014-10-31 17:42:38 -0600

    Rebase from 'release/indigo/rviz_visual_tools'

--- include/rviz_visual_tools/rviz_visual_tools.h
+++ include/rviz_visual_tools/rviz_visual_tools.h
@@ -89,7 +89,7 @@ enum scales { XXSMALL,
               XLARGE,
               XXLARGE };
 
-class VisualTools
+class RvizVisualTools
 {
 protected:
 
@@ -144,12 +144,12 @@ public:
    * \param base_frame - common base for all visualization markers, usually "/world" or "/odom"
    * \param marker_topic - rostopic to publish markers to - your Rviz display should match
    */
-  VisualTools(const std::string& base_frame,
+  RvizVisualTools(const std::string& base_frame,
               const std::string& marker_topic = RVIZ_MARKER_TOPIC);
   /**
    * \brief Deconstructor
    */
-  ~VisualTools() {};
+  ~RvizVisualTools() {};
 
   /**
    * \brief Tell Rviz to clear all markers on a particular display. Note: only works on ROS Indigo and newer
@@ -172,6 +172,14 @@ public:
    * \brief Load publishers as needed
    */
   void loadMarkerPub();
+
+  /**
+   * \brief Wait until at least one subscriber connects to a publisher
+   * \param pub - the publisher to check for subsribers
+   * \param wait_time - time to wait for subscriber to be availnle before throwing warning
+   * \return true on successful connection
+   */
+  bool waitForSubscriber(const ros::Publisher &pub, const double &wait_time = 1.0);
 
   /**
    * \brief Return if we are in verbose mode
@@ -493,6 +501,12 @@ public:
   void generateRandomPose(geometry_msgs::Pose& pose);
 
   /**
+   * \brief Create a pose of position (0,0,0) and quaternion (0,0,0,1)
+   * \param Pose to fill in
+   */
+  void generateEmptyPose(geometry_msgs::Pose& pose);
+
+  /**
    * \brief Get random between min and max
    */
   static double dRand(double min, double max);
@@ -506,8 +520,8 @@ public:
 
 }; // class
 
-typedef boost::shared_ptr<VisualTools> VisualToolsPtr;
-typedef boost::shared_ptr<const VisualTools> VisualToolsConstPtr;
+typedef boost::shared_ptr<RvizVisualTools> RvizVisualToolsPtr;
+typedef boost::shared_ptr<const RvizVisualTools> RvizVisualToolsConstPtr;
 
 } // namespace
 
